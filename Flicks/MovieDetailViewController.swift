ab797a7d430a976d73cf944f346fd53b54689cb7
Utkarsh Sengar <utkarsh2012@gmail.com>
2017-04-01 10:12:40 -0700

    Added detail view

---
+++ Flicks/MovieDetailViewController.swift
@@ -0,0 +1,55 @@
+//
+//  MovieDetailViewController.swift
+//  Flicks
+//
+//  Created by Utkarsh Sengar on 4/1/17.
+//  Copyright © 2017 Area42. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+class MovieDetailViewController: UIViewController {
+
+    @IBOutlet weak var bigPosterImage: UIImageView!
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var movieOverview: UILabel!
+    @IBOutlet weak var scroll: UIScrollView!
+    @IBOutlet weak var infoView: UIView!
+    
+    var movie: NSDictionary!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scroll.contentSize = CGSize(width: scroll.frame.size.width, height: infoView.frame.origin.y + infoView.frame.size.height)
+        movieTitle.text = movie["title"] as? String
+        movieOverview.text = movie["overview"] as? String
+        movieOverview.sizeToFit()
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w500"
+        if let posterPath = movie["poster_path"] as? String{
+            let imgUrl = NSURL(string: baseUrl + posterPath)
+            bigPosterImage.setImageWith(imgUrl as URL!)
+        }
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -46,17 +46,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         if let movies = movies {
             return movies.count
@@ -74,15 +63,26 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
-        let posterPath = movie["poster_path"] as! String
-
+        
         let baseUrl = "https://image.tmdb.org/t/p/w500"
-        let imgUrl = NSURL(string: baseUrl + posterPath)
+        if let posterPath = movie["poster_path"] as? String{
+            let imgUrl = NSURL(string: baseUrl + posterPath)
+            cell.posterView.setImageWith(imgUrl as URL!)
+        }
         
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
-        cell.posterView.setImageWith(imgUrl as URL!)
         
         return cell
     }
+    
+     // MARK: - Navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)
+        
+        let movie = movies![(indexPath?.row)!]
+        let detailViewController = segue.destination as! MovieDetailViewController
+        detailViewController.movie = movie
+     }
 }
